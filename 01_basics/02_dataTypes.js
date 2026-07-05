8da00cdb1e8065283c922495b0bf7a321c15776c
nikhilkumar95f <nikhil9508821695@gmail.com>
2026-07-05 10:19:38 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all code as modern JavaScript
+
+// alert(3 + 3) //we are using node.js , browser
+
+console.log(3
+     + 
+     3) //code readability should be high
+
+     console.log("Nikhil")
+     
+
+let name = "Nikhil"
+let age = 20
+let isLoggedIn = true
+let state ;
+
+
+//number =>2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+;
