134dc932bc4444ee5022b35993f0324fbf743403
nikhilkumar95f <nikhil9508821695@gmail.com>
2026-07-11 11:03:30 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,3 +38,33 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("Nikhil"))
 // console.log(loginUserMessage())
 
+function calculateCartPrice(...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Nikhil",
+    age: 20,
+    
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and age is ${anyobject.age}`);  
+}
+
+// handleObject(user)
+ handleObject({
+    username: "Nikhil",
+    age: 20,
+ })
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,600]));
