1d3e89410bd890462cbab7f2093d793c9fa72f78
nikhilkumar95f <nikhil9508821695@gmail.com>
2026-07-15 19:36:24 +0530

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -2,9 +2,57 @@ const user = {
     username: "hitesh",
     price: 999,
 
-    
     welcomMessage: function(){
         console.log(`${this.username} , welcome to website`);
-        
+        console.log(this);
+        }
+
 }
+
+// user.welcomMessage()
+// user.username = "sam"
+// user.welcomMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "Nikhil"
+//      console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function (){
+//     let username = "Nikhil"
+//     console.log(this.username);
+// }
+
+// chai()
+
+const chai =  () =>{
+    let username = "Nikhil"
+    console.log(this.username);
 }
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2 
+// }
+
+// console.log(addTwo(3, 4));
+
+
+// const addTwo = (num1, num2) =>   num1 + num2 
+
+// const addTwo = (num1, num2) =>  ( num1 + num2 )
+
+const addTwo = (num1, num2) =>  ({usrname: "Nikhil"})
+
+
+console.log(addTwo(3, 4));
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
